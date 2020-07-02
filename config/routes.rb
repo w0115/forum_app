f4997ddaa51dc7ae0be0461b847d43eef7dc6f75
wagou <wago.misaki@viva-eureka.co.jp>
2020-07-02 05:02:52 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'users#new'
   
   get  '/signup',  to: 'users#new'
+  
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
