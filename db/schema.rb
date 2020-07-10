c78fbffef61c7c8011891bae2b77e4dcaf98bef6
wagou <wago.misaki@viva-eureka.co.jp>
2020-07-10 05:07:57 +0000

    試しに保存(M db/migrate/20200702011817_create_users.rb  M db/schema.rb)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200707065252) do
+ActiveRecord::Schema.define(version: 20200710041047) do
 
   create_table "blacklists", force: :cascade do |t|
     t.integer "user_id"
@@ -38,10 +38,10 @@ ActiveRecord::Schema.define(version: 20200707065252) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
-    t.boolean "admin", default: false
     t.boolean "flag", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin"
   end
 
 end
